52353156f64ab8d845647513753ced6ebd66ce39
Guillaumebeuzeboc <Guillaumebeuzeboc@users.noreply.github.com>
2024-01-11 16:02:49 +0000

    ✅ Ready to clone and code.

---
+++ cos_registration_server/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for cos_registration_server."""
+
+from cos_registration_server.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- cos_registration_server/cli.py
+++ cos_registration_server/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for cos_registration_server project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m cos_registration_server` and `$ cos_registration_server `.
 
     This is your program's entry point.
 
