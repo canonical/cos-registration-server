c486c7b8655e1dbeee11e0ce245a404cd96ff3fc
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-01-15 14:49:06 +0100

    feat(rename registration into device)

---
+++ cos_registration_server/devices/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Device(models.Model):
+
+    uid = models.CharField(max_length=200)
+    creation_date = models.DateTimeField("creation date", auto_now_add=True)
+    address = models.GenericIPAddressField("device IP")
+
+    def __str__(self):
+        return self.uid
