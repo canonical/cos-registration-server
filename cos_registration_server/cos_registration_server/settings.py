196ad060102220772efb0990b7f76799f0be8f16
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-01-18 17:21:03 +0100

    feat(secret_key): don't use hardcoded secret key

--- cos_registration_server/cos_registration_server/settings.py
+++ cos_registration_server/cos_registration_server/settings.py
@@ -10,19 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-$b#5b(2h9_%p439=#ev0!dkde9wqt=rgoc!jvi-y93^@+wcvw8"
-)
+try:
+    SECRET_KEY = os.environ["SECRET_KEY_DJANGO"]
+except KeyError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
