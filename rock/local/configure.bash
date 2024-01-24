eb01c8466fb3c874d6f9439689a5da7b98b15919
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-01-24 15:49:02 +0100

    CI(rock)

--- rock/local/configure.bash
+++ rock/local/configure.bash
@@ -2,7 +2,7 @@
 
 cd /lib/python3.10/site-packages/cos_registration_server
 
-export DATABASE_BASE_DIR_DJANGO="~/server_data"
+export DATABASE_BASE_DIR_DJANGO="/server_data"
 
 # migrate the database
 /bin/python3 manage.py migrate
--- rock/local/launcher.bash
+++ rock/local/launcher.bash
@@ -3,7 +3,7 @@
 # necessary for daphne as the option --root-path doesn't work
 cd /lib/python3.10/site-packages/cos_registration_server
 
-export DATABASE_BASE_DIR_DJANGO="~/server_data"
-export SECRET_KEY_DJANGO=$(cat ~/server_data/secret_key)
+export DATABASE_BASE_DIR_DJANGO="/server_data"
+export SECRET_KEY_DJANGO=$(cat /server_data/secret_key)
 
 /bin/daphne cos_registration_server.asgi:application
