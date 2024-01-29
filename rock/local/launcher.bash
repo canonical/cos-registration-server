ec034ff951378db786b15e487221c3964ceb0411
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-01-29 12:38:39 +0100

    comment

--- rock/local/launcher.bash
+++ rock/local/launcher.bash
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 
-# necessary for daphne as the option --root-path doesn't work
 cd /lib/python3.10/site-packages/cos_registration_server
 
 export DATABASE_BASE_DIR_DJANGO="/server_data"
