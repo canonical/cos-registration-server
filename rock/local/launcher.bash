0741415d6e7fb9abd66c92cc1ae1d9e955793a64
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-01-29 15:30:43 +0100

    fix(gunicorn): use gunicorn instead of daphne

--- rock/local/launcher.bash
+++ rock/local/launcher.bash
@@ -5,4 +5,4 @@ cd /lib/python3.10/site-packages/cos_registration_server
 export DATABASE_BASE_DIR_DJANGO="/server_data"
 export SECRET_KEY_DJANGO=$(cat /server_data/secret_key)
 
-/bin/daphne --root-path ${ROOT_PATH_DAPHNE} -b 0.0.0.0 cos_registration_server.asgi:application
+gunicorn --bind 0.0.0.0:8000 cos_registration_server.wsgi
