ae527a4b49e6d6fd4413030e118bfdf2acdcb2ea
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2024-01-31 17:02:48 +0100

    fix(settings): manage static files with script_name

--- cos_registration_server/cos_registration_server/settings.py
+++ cos_registration_server/cos_registration_server/settings.py
@@ -134,8 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "static/"
+STATIC_URL = os.getenv("SCRIPT_NAME", "") + "/static/"
+
+STATIC_ROOT = BASE_DIR / "static/"
+
+WHITENOISE_STATIC_PREFIX = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
