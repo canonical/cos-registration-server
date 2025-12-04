bf3d7d263294c75fc0df7c10737575c9ae86324a
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2025-12-04 15:40:38 +0100

    fix(rock): no need for the DATABASE_BASE_DIR_DJANGO anymore

--- rock/local/configure.bash
+++ rock/local/configure.bash
@@ -2,7 +2,6 @@
 
 cd /lib/python3.10/site-packages/cos_registration_server
 
-export DATABASE_BASE_DIR_DJANGO="/server_data"
 export SECRET_KEY_DJANGO=$(cat /server_data/secret_key)
 
 # migrate the database
