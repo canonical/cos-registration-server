133d2d3d9e5d89db6e2212f64bf10b6d3f7f902b
Guillaumebeuzeboc <guillaume.beuzeboc@gmail.com>
2026-01-26 17:27:21 +0100

    fix(settings): re defined the WHITENOISE_STATIC_PREFIX

--- cos_registration_server/cos_registration_server/settings.py
+++ cos_registration_server/cos_registration_server/settings.py
@@ -155,6 +155,8 @@ STATIC_URL = os.getenv("SCRIPT_NAME", "") + "/static/"
 
 STATIC_ROOT = BASE_DIR / "static/"
 
+WHITENOISE_STATIC_PREFIX = "static/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
